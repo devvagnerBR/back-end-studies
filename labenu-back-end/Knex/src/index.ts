385b499f02998f97f5bedbbe7174248624f88113
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-08 09:56:30 -0300

    endpoints PUT, DELETE

--- labenu-back-end/Knex/src/index.ts
+++ labenu-back-end/Knex/src/index.ts
@@ -9,6 +9,27 @@ app.use( express.json() );
 app.use( cors() );
 
 
+// GET ALL ACTORS
+
+app.get( "/actor", async ( req: Request, res: Response ): Promise<void> => {
+
+    try {
+
+        const result = await connection.raw( `
+        SELECT * FROM Actor;
+
+    `)
+
+        res.status( 200 ).send( { message: result[0] } )
+
+    } catch ( error: any ) {
+
+        res.status( 500 ).send( error.sqlMessage || error.message )
+
+    }
+} )
+
+
 // ADD ACTOR
 
 app.post( "/actor", async ( req: Request, res: Response ): Promise<void> => {
@@ -34,28 +55,51 @@ app.post( "/actor", async ( req: Request, res: Response ): Promise<void> => {
 } )
 
 
-// GET ALL ACTORS
 
-app.get( "/actor", async ( req: Request, res: Response ): Promise<void> => {
+
+//  UPDATE ACTOR
+
+app.put( "/actor/:id", async ( req: Request, res: Response ): Promise<void> => {
 
     try {
 
-        const result = await connection.raw( `
-        SELECT * FROM Actor;
+        await connection( "Actor" )
 
-    `)
+            .update( {
 
-        res.status( 200 ).send( { message: result[0] } )
-        
+                name: req.body.name,
+                salary: req.body.salary,
+                birth_date: req.body.birthDate,
+                gender: req.body.gender,
+
+            } ).where( { id: req.params.id } )
+
+        res.status( 200 ).send( { id: req.params.id } )
+
     } catch ( error: any ) {
 
         res.status( 500 ).send( error.sqlMessage || error.message )
 
     }
+
 } )
 
 
-//
+// DELETE ACTOR
+
+app.delete( '/actor/:id', async ( req: Request, res: Response ): Promise<void> => {
+
+    try {
+
+        await connection( "Actor" ).where( { id: req.params.id } ).delete()
+        res.status( 200 ).send( "Ator deletado com sucesso" )
+
+    } catch ( error: any ) {
+
+        res.status( 500 ).send( error.sqlMessage || error.message )
+    }
+
+} )
 
 
 
@@ -70,4 +114,3 @@ const server = app.listen( 3003, () => {
 } );
 
 
-// 46MIN
