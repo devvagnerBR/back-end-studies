8174138f8573f40af44200a51f147ba1b297a083
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-08 10:12:09 -0300

    configurando dot.env

--- labenu-back-end/Knex/src/connections.ts
+++ labenu-back-end/Knex/src/connections.ts
@@ -1,15 +1,21 @@
 import knex from 'knex'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 const connection = knex( {
+
     client: "mysql",
+
     connection: {
-        host: "localhost",
+
+        host: process.env.DB_HOST,
         port: 3306,
-        user: "devvagnerBR",
-        password: "scotow21",
-        database: "estudos_labenu",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
         multiStatements: true
+
     }
 } )
 
