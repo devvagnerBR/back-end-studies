777a3827beddc431bc9875ce473722e79209af89
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-09 11:14:48 -0300

    paginação

--- labenu-back-end/Filtros-Ordenacao-e-Paginacao/src/endpoints/getAllRecipes.ts
+++ labenu-back-end/Filtros-Ordenacao-e-Paginacao/src/endpoints/getAllRecipes.ts
@@ -2,14 +2,9 @@ import { Request, Response } from "express"
 import { connection } from "../data/connection"
 import { recipe } from "../types"
 
-export async function getAllRecipes(
+export async function getAllRecipes( req: Request, res: Response ): Promise<void> { // a função não precisa retornar nada, ela vai enviar uma resposta somente.
 
-   req: Request,
-   res: Response
-
-): Promise<void> { // a função não precisa retornar nada, ela vai enviar uma resposta somente.
-
-   let statusCode = 400
+   let statusCode = 400 // definindo o erro 
 
    try {
 
@@ -17,43 +12,38 @@ export async function getAllRecipes(
       let sort = req.query.sort as string;
       let order = req.query.order as string;
 
-      if ( !title ) {
-         title = "%" // retorna tudo
-      }
-      if ( !sort ) {
-         sort = "title" 
-      }
-      if ( sort === "createdAt" ) { // caso o front-end digite em camelCase ele converte para snake_case
-         sort = "created_at"
-      }
-      if ( sort !== "title" && sort !== "created_at" ) { // se não for passado no sorte nem title nem created_at retorna ordenado pelo titulo
-         sort = "title"
-      }
+      let size = Number( req.query.size );
+      let page = Number( req.query.page );
+
+      if ( !title ) title = "%"; // retorna tudo
+      if ( !sort ) sort = "title";
+      if ( order?.toUpperCase() !== "ASC" && "DESC" ) order = "ASC";
+      if ( sort === "createdAt" ) sort = "created_at"; // caso o front-end digite em camelCase ele converte para snake_case
+      if ( sort !== "title" && sort !== "created_at" ) sort = "title";  // se não for passado no sorte nem title nem created_at retorna ordenado pelo titulo
+
+      if ( isNaN( size ) || size < 1 ) size = 10;
+      if ( isNaN( page ) || page < 1 ) page = 1;
+
+      let offset = size * ( page - 1 )
 
       const result = await connection( "aula49_recipes" )
          .where( "title", "like", `%${title}%` )
          .orderBy( sort, order )
-
-      /* 
-      ou
-      connection.raw(`
-      SELECT * FROM recipes 
-      WHERE title LIKE "%${title}%"
-      `)
-      */
-
-      //recipes?title=sopa&sort=title&order=DESC
-      // SELECT * FROM recipes WHERE title LIKE "%sopa%" ORDER BY title DESC
+         .limit( size )
+         .offset( offset )
 
       if ( result.length < 1 ) {
          statusCode = 404
          throw new Error( "Nenhuma receita encontrada" )
       }
+
       const recipes = result.map( toRecipe )
       res.status( 200 ).send( recipes )
 
    } catch ( error: any ) {
+
       res.status( statusCode ).send( error.message )
+
    }
 
 }
