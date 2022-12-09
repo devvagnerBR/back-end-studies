e3dda8c3aa0e0fd66c1b824aab626ccfa678abdf
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-09 10:10:59 -0300

    ordenação

--- labenu-back-end/Filtros-Ordenacao-e-Paginacao/src/endpoints/getAllRecipes.ts
+++ labenu-back-end/Filtros-Ordenacao-e-Paginacao/src/endpoints/getAllRecipes.ts
@@ -9,22 +9,53 @@ export async function getAllRecipes(
 
 ): Promise<void> { // a função não precisa retornar nada, ela vai enviar uma resposta somente.
 
+   let statusCode = 400
+
    try {
 
       let title = req.query.title as string;
+      let sort = req.query.sort as string;
+      let order = req.query.order as string;
 
-      const result = await
-         connection( "aula49_recipes" )
-            .where( "title", "like", `%${title}%` )
+      if ( !title ) {
+         title = "%" // retorna tudo
+      }
+      if ( !sort ) {
+         sort = "title" 
+      }
+      if ( sort === "createdAt" ) { // caso o front-end digite em camelCase ele converte para snake_case
+         sort = "created_at"
+      }
+      if ( sort !== "title" && sort !== "created_at" ) { // se não for passado no sorte nem title nem created_at retorna ordenado pelo titulo
+         sort = "title"
+      }
 
+      const result = await connection( "aula49_recipes" )
+         .where( "title", "like", `%${title}%` )
+         .orderBy( sort, order )
+
+      /* 
+      ou
+      connection.raw(`
+      SELECT * FROM recipes 
+      WHERE title LIKE "%${title}%"
+      `)
+      */
+
+      //recipes?title=sopa&sort=title&order=DESC
+      // SELECT * FROM recipes WHERE title LIKE "%sopa%" ORDER BY title DESC
+
+      if ( result.length < 1 ) {
+         statusCode = 404
+         throw new Error( "Nenhuma receita encontrada" )
+      }
       const recipes = result.map( toRecipe )
       res.status( 200 ).send( recipes )
 
-   } catch ( error ) {
-
-      res.status( 500 ).send( "Internal server error" )
-
+   } catch ( error: any ) {
+      res.status( statusCode ).send( error.message )
    }
+
 }
 
 const toRecipe = ( input: any ): recipe => {
