c85f4c2e6eb1db3c9e1f035ed3fe9cb909c9b735
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-10 10:11:24 -0300

    index.ts criado

--- back-end/To Do List/src/database/tableNames.ts
+++ back-end/To Do List/src/database/tableNames.ts
@@ -1,7 +1,3 @@
 export const TABLE_TO_DO_LIST_USER = "TodoListUser"
 export const TABLE_TO_DO_LIST_TASK = "TodoListTask"
 export const TABLE_TO_DO_RESPONSIBLE_USER_TASK_RELATION = "TodoResponsibleUserTaskRelation"
-
-
-
-
