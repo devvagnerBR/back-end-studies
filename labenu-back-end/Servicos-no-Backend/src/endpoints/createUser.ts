74ca429cf0bfd656323134c31c3a204fd71240d9
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-11 15:04:19 -0300

    funcionalidade de mandar email

--- labenu-back-end/Servicos-no-Backend/src/endpoints/createUser.ts
+++ labenu-back-end/Servicos-no-Backend/src/endpoints/createUser.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from "express" // lembrar de sempre importar o req,res
 import { getAddressInfo } from './../services/getAddressInfo';
+import transporter from './../services/mailTransporter';
 
 export const createUser = async ( req: Request, res: Response ) => {
 
     try {
 
-        const { zipcode } = req.body
+        const { zipcode, name, email } = req.body
         const address = await getAddressInfo( zipcode )
 
         if ( !address ) {
             throw new Error( 'Error on get address!' )
         }
+
+        await transporter.sendMail( {
+            from: process.env.NODEMAILER_USER,
+            to: email,
+            subject: "Conta criada com sucesso",
+            text: "Parabéns conta criada com sucesso",
+            html: `<h1>Parabéns ${name}, seu acesso foi liberado na meta_dev😊</h1>`
+        } )
 
         res.send( address )
 
