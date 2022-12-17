d6cebdc4b76352b33bbdef9e6b8389c462fa3cbd
devvagnerBR <wagnerluizsg@outlook.com.br>
2022-12-17 15:07:02 -0300

    alterando arquivos

---
+++ labenu-back-end/arquitetura-limpa/src/2_business/UserBusiness.ts
@@ -0,0 +1,47 @@
+import { UserDatabase } from "../3_data/mySQL/UserDatabase";
+import { CustomError, InvalidEmail, InvalidName } from "../error/customError";
+import { user, UserInputDTO } from "../model/user";
+import { generateId } from "../services/generateId";
+
+
+export class UserBusiness {
+
+  constructor( private userDatabase: UserDatabase ) {
+
+  }
+
+  public createUser = async ( input: UserInputDTO ) => {
+    try {
+      const { name, nickname, email, password } = input;
+
+      if ( !name || !nickname || !email || !password ) {
+        throw new CustomError(
+          400,
+          'Preencha os campos "name","nickname", "email" e "password"'
+        );
+      }
+
+      if ( name.length < 4 ) {
+        throw new InvalidName();
+      }
+
+      if ( !email.includes( "@" ) ) {
+        throw new InvalidEmail();
+      }
+
+      const id: string = generateId();
+
+      const user: user = {
+        id,
+        name,
+        nickname,
+        email,
+        password,
+      };
+      // const userDatabase = new UserDatabase()
+      await this.userDatabase.insertUser( user );
+    } catch ( error: any ) {
+      throw new Error( error.message );
+    }
+  };
+}
